6d33ba3dda8156da24d542ed4d48534b24d96b93
Steven Tey <stevensteel97@gmail.com>
2023-01-20 12:45:07 -0600

    update tweet link

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       >
         <motion.a
           variants={FADE_DOWN_ANIMATION_VARIANTS}
-          href="https://twitter.com/steventey/status/1613928948915920896"
+          href="https://twitter.com/steventey/status/1616505632001232896"
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
