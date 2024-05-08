ba65a55ca0e5c1636e5131b8630dad13bbcd949a
Ajay <ajayvignesh01@gmail.com>
2024-05-08 07:04:27 -0400

    feat: supabase app router clients

---
+++ lib/supabase/admin.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+export const createAdminClient = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  );
