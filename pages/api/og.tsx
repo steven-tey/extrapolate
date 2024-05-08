9fba9c095c5adb1773a3a10e848ebc78598977fd
Ajay <ajayvignesh01@gmail.com>
2024-05-07 23:23:29 -0400

    fix: ImageResponse import (@vercel/og)

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { NextRequest } from "next/server";
-import {ImageResponse} from "next/og";
+import {ImageResponse} from "@vercel/og";
 
 export const config = {
   runtime: "edge",
