3c27a3116cc2fbf91e6174f5eb8344aea7c94347
Ajay <ajayvignesh01@gmail.com>
2024-05-16 15:06:01 -0400

    init: shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,98 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
 const config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  prefix: "",
   future: {
     hoverOnlyWhenSupported: true,
   },
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        // Tooltip - Extrapolate Old
+        "slide-up-fade": {
+          "0%": { opacity: "0", transform: "translateY(6px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "slide-down-fade": {
+          "0%": { opacity: "0", transform: "translateY(-6px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        // Tooltip - Extrapolate Old
+        "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+      },
       fontFamily: {
         display: ["var(--font-clash)"],
         default: ["var(--font-inter)", "system-ui", "sans-serif"],
       },
-      animation: {
-        // Tooltip
-        "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-        "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-      },
-      keyframes: {
-        // Tooltip
-        "slide-up-fade": {
-          "0%": { opacity: '0', transform: "translateY(6px)" },
-          "100%": { opacity: '1', transform: "translateY(0)" },
-        },
-        "slide-down-fade": {
-          "0%": { opacity: '0', transform: "translateY(-6px)" },
-          "100%": { opacity: '1', transform: "translateY(0)" },
-        },
-      },
     },
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
+
+    // Extrapolate Old
     plugin(({ addVariant }) => {
       addVariant("radix-state-checked", '&[data-state="checked"]');
       addVariant("radix-state-unchecked", '&[data-state="unchecked"]');
@@ -46,4 +102,4 @@ const config = {
   ],
 } satisfies Config;
 
-export default config
+export default config;
