1e0d01a2c04dba6bc802f0b7adae25460fd41567
Ajay <ajayvignesh01@gmail.com>
2024-05-21 17:30:48 -0400

    feat: new pricing modal

--- components/layout/checkout-dialog.tsx
+++ components/layout/checkout-dialog.tsx
@@ -29,6 +29,18 @@ import { useTransition } from "react";
 import { checkout } from "@/app/actions/checkout";
 import { LoadingDots } from "@/components/shared/icons";
 import { useUserDataStore } from "@/components/layout/navbar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CircleCheck } from "lucide-react";
+import { BackgroundGradient } from "@/components/aceternity-ui/background-gradient";
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 type CheckoutDialogStore = {
   open: boolean;
@@ -36,7 +48,7 @@ type CheckoutDialogStore = {
 };
 
 export const useCheckoutDialog = create<CheckoutDialogStore>((set) => ({
-  open: false,
+  open: true,
   setOpen: (open) => set(() => ({ open: open })),
 }));
 
@@ -44,10 +56,13 @@ export function CheckoutDialog() {
   const supabase = createClient();
 
   const { data: products } = useSWRImmutable("get_products", async () => {
+    // TODO: sort by price in rpc, lowest --> greatest
     const { data: products, error } = await supabase.rpc("get_products");
 
     return products;
   });
+
+  const sortedProducts = products?.sort((a: any, b: any) => a.price - b.price);
 
   const [open, setOpen] = useCheckoutDialog((s) => [s.open, s.setOpen]);
   const userData = useUserDataStore((s) => s.userData);
@@ -77,14 +92,7 @@ export function CheckoutDialog() {
 
           <Separator />
 
-          {/* Buttons */}
-          <div className="flex flex-col space-y-4 bg-muted px-16 py-8">
-            {products
-              ?.sort((a: any, b: any) => a.price - b.price)
-              .map((product) => (
-                <CheckoutButton key={product.id} product={product} />
-              ))}
-          </div>
+          <Pricing products={sortedProducts} />
 
           <DialogFooter className="bg-muted pb-8 text-sm text-muted-foreground sm:justify-center">
             {`You currently have ${userData?.credits} credits and can generate ${Math.floor((userData?.credits || 0) / 10)} images`}
@@ -117,14 +125,7 @@ export function CheckoutDialog() {
 
         <Separator />
 
-        {/* Buttons */}
-        <div className="flex flex-col space-y-4 bg-muted px-4 py-8">
-          {products
-            ?.sort((a: any, b: any) => a.price - b.price)
-            .map((product) => (
-              <CheckoutButton key={product.id} product={product} />
-            ))}
-        </div>
+        <Pricing products={sortedProducts} />
 
         <DrawerFooter className="bg-muted">
           <p className="text-center text-sm text-muted-foreground">{`You currently have ${userData?.credits} credits and can generate ${Math.floor((userData?.credits || 0) / 10)} images`}</p>
@@ -137,7 +138,85 @@ export function CheckoutDialog() {
   );
 }
 
-function CheckoutButton({ product }: { product: Product | null }) {
+function Pricing({ products }: { products: Product[] | null | undefined }) {
+  return (
+    <div className="flex w-full flex-col items-center justify-center space-y-4 bg-muted px-4 py-8">
+      {products?.map((product, index) => {
+        if (index === 1) {
+          return (
+            <BackgroundGradient
+              key={index}
+              containerClassName={cn(
+                "rounded-lg bg-card text-card-foreground shadow-sm",
+                "rounded-2xl w-full",
+                "p-[2.5px]",
+              )}
+              className={cn(
+                "flex flex-row justify-between rounded-[14px] bg-background p-3",
+              )}
+            >
+              <div>
+                <CardHeader className="p-0 pb-3">
+                  <CardTitle className="flex flex-row items-center">
+                    {product.name}
+                    <Badge className="ml-2 scale-75">POPULAR</Badge>
+                  </CardTitle>
+                  <CardDescription>{product.description}</CardDescription>
+                </CardHeader>
+                <CardContent className="p-0">
+                  <ul className="flex flex-row space-x-2">
+                    <li className="flex flex-row items-center">
+                      <CircleCheck className="mr-2 size-4" />
+                      {`${product.credits} credits`}
+                    </li>
+                    <li className="flex flex-row items-center">
+                      <CircleCheck className="mr-2 size-4" />
+                      {`$${product.price}`}
+                    </li>
+                  </ul>
+                </CardContent>
+              </div>
+              <CardFooter className="flex items-center justify-center p-0">
+                <CheckoutButton product={product} />
+              </CardFooter>
+            </BackgroundGradient>
+          );
+        }
+
+        return (
+          <Card
+            className="flex w-full flex-row justify-between rounded-2xl p-3"
+            key={index}
+          >
+            <div>
+              <CardHeader className="p-0 pb-3">
+                <CardTitle>{product.name}</CardTitle>
+                <CardDescription>{product.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="p-0">
+                <ul className="flex flex-row space-x-2">
+                  <li className="flex flex-row items-center">
+                    <CircleCheck className="mr-2 size-4" />
+                    {`${product.credits} credits`}
+                  </li>
+                  <li className="flex flex-row items-center">
+                    <CircleCheck className="mr-2 size-4" />
+                    {`$${product.price}`}
+                  </li>
+                </ul>
+              </CardContent>
+            </div>
+            <CardFooter className="flex items-center justify-center p-0">
+              <CheckoutButton product={product} />
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
+
+function CheckoutButton({ product }: { product: Product | null | undefined }) {
   const [isPending, startTransition] = useTransition();
 
   const checkoutWithProps = checkout.bind(null, {
@@ -147,8 +226,7 @@ function CheckoutButton({ product }: { product: Product | null }) {
 
   return (
     <Button
-      variant="outline"
-      className="w-full focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      className="w-24 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
       onClick={() => {
         startTransition(async () => {
           await checkoutWithProps();
@@ -160,7 +238,7 @@ function CheckoutButton({ product }: { product: Product | null }) {
         <LoadingDots color="#808080" />
       ) : (
         <>
-          <p>{`${product?.credits} credits - $${product?.price}`}</p>
+          <p>Purchase</p>
         </>
       )}
     </Button>
